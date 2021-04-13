7589e78d2b3d8afb0d552b2067120db71918e981
Sylvie Bielak <sbielak2021@jpkeefehs.org>
2021-04-13 13:16:14 -0400

    Added dessert

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -10,6 +10,7 @@ namespace Hello_World
             Console.WriteLine("Welcome to GitHub");
             Console.WriteLine("My name is Sylvie");
             Console.WriteLine("My favorite food is Bread");
+            Console.WriteLine("My favorite dessert is chocolate");
         }
     }
 }
